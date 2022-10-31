dbf98400f91a877b0cf47fabb99b3c1dddc5b967
BhaskarMantralaHub <97362616+BhaskarMantralaHub@users.noreply.github.com>
2022-10-30 20:14:10 -0700

    new changes committed

--- src/first-script.ts
+++ src/first-script.ts
@@ -1,4 +1,16 @@
-import { USER } from './config/base.config';
+interface Item {
+  quantity: number;
+  itemPrice: number;
+}
 
-console.log('Hello ', USER);
-console.log(`Hello ${USER}`);
+function calculateTotal(item: Item): number {
+  const basePrice = item.quantity * item.itemPrice;
+  if (basePrice > 1000) {
+    return basePrice * 0.95;
+  } else {
+    return basePrice * 0.98;
+  }
+}
+
+const totalPrice = calculateTotal({ itemPrice: 100, quantity: 10 });
+console.log(totalPrice);
