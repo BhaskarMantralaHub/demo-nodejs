7b45574af29439d0c2c6ff693ca568afbdcb41df
BhaskarMantralaHub <97362616+BhaskarMantralaHub@users.noreply.github.com>
2023-01-04 20:59:48 -0800

    satisfies-operator.ts

--- src/usages/satisfies-operator.ts
+++ src/usages/satisfies-operator.ts
@@ -1,33 +1,33 @@
-interface ILoanTerms {
-  apr: string;
-  amount: string;
-}
-
-interface ILoan {
-  terms: string | ILoanTerms;
-}
-
-//without satisfies operator
-const withOutSatisfiesOperator: ILoan = {
-  terms: 'String value',
-};
-
-// console.log(withOutSatisfiesOperator.terms.toLowerCase()); //cannot access string props and methods
-
-//**************************************************************************************
-
-//with satisfies operator
-const withSatisfiesOperator = {
-  terms: 'String value',
-} satisfies ILoan;
-
-const withSatisfiesOperator1 = {
-  terms: {
-    apr: '10',
-    amount: '2000',
-  },
-} satisfies ILoan;
-
-console.log(withSatisfiesOperator.terms.length);
-
-console.log(withSatisfiesOperator1.terms.amount);
+// interface ILoanTerms {
+//   apr: string;
+//   amount: string;
+// }
+//
+// interface ILoan {
+//   terms: string | ILoanTerms;
+// }
+//
+// //without satisfies operator
+// const withOutSatisfiesOperator: ILoan = {
+//   terms: 'String value',
+// };
+//
+// // console.log(withOutSatisfiesOperator.terms.toLowerCase()); //cannot access string props and methods
+//
+// //**************************************************************************************
+//
+// //with satisfies operator
+// const withSatisfiesOperator = {
+//   terms: 'String value',
+// } satisfies ILoan;
+//
+// const withSatisfiesOperator1 = {
+//   terms: {
+//     apr: '10',
+//     amount: '2000',
+//   },
+// } satisfies ILoan;
+//
+// console.log(withSatisfiesOperator.terms.length); // string operations
+//
+// console.log(withSatisfiesOperator1.terms.amount); //object operations
